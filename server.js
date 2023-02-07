2d3b81bdbbc1f429e8c28977f8c4382e0ef5de1a
Glitch (spectacled-lofty-stingray) <none>
2023-02-07 19:53:17 +0000

    ❤️🚣 Checkpoint ./server.js:54310601/32

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 myDB(async (client) => {
-  const myDataBase = await client.db('database').collection('users');
+  const myDataBase = await client.db('users_db').collection('users');
 
   app.route('/').get((req, res) => {
     res.render('pug', {
