0784aa6572b854d283f6f3900c55f3cd5f6e82ca
Glitch (spectacled-lofty-stingray) <none>
2023-02-07 18:26:41 +0000

    🧀🏚 Checkpoint ./server.js:54310601/3997

--- server.js
+++ server.js
@@ -1,97 +1,59 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const ObjectID = require('mongodb').ObjectID;
+
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
-fccTesting(app); //For FCC testing purposes
+fccTesting(app); // For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'pug');
-app.set('views', './views/pug');
 
-let session = require('express-session');
-let passport = require('passport');
-let ObjectID = require('mongodb');
-const mongo = require('mongodb').MongoClient;
-
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
-  cookie: { secure: false }
-}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.route('/').get((req, res) => {
-res.render('index', { title: 'Hello', message: 'Please log in' });
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
-});
-
-
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
-
-passport.deserializeUser((id, done) => {
-  myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-    done(null, null);
-  });
-});
-myDB(async client => {
-  const myDataBase = await client.db('database').collection('users');
-
-  // Be sure to change the title
-  app.route('/').get((req, res) => {
-    // Change the response to render the Pug template
-    res.render('index', {
-      title: 'Connected to Database',
-      message: 'Please login'
-    });
-  });
-
-  // Serialization and deserialization here...
-
-  // Be sure to add this...
-}).catch(e => {
-  app.route('/').get((req, res) => {
-    res.render('index', { title: e, message: 'Unable to connect to database' });
-  });
-});
-// app.listen out here...
-let uri = 
-    "mongodb+srv://mkn:" + 
-    process.env.PW + 
-    "@cluster0.iainz.mongodb.net/?retryWrites=true&w=majority";
-
-myDB(async client => {
+myDB(async (client) => {
   const myDataBase = await client.db('advancednode_users').collection('users');
 
-  // Be sure to change the title
   app.route('/').get((req, res) => {
-    // Change the response to render the Pug template
-    res.render('index', {
+    res.render('pug', {
       title: 'Connected to Database',
-      message: 'Please login'
+      message: 'Please login',
     });
   });
 
-  // Serialization and deserialization here...
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
 
-  // Be sure to add this...
-}).catch(e => {
+  passport.deserializeUser(async (id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
+}).catch((e) => {
   app.route('/').get((req, res) => {
-    res.render('index', { title: e, message: 'Unable to connect to database' });
+    res.render('pug', { title: e, message: 'Unable to login' });
   });
 });
-// app.listen out here...
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Listening on port ' + process.env.PORT);
+});
