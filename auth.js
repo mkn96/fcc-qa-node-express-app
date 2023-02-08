9165a1de78370bdeed4b721ca08a9b05eb47d59e
Glitch (spectacled-lofty-stingray) <none>
2023-02-08 18:10:53 +0000

    📚🕍 Checkpoint ./server.js:54310601/6865 ./route.js:54310601/8331 ./routes.js:54310601/39 ./auth.js:54310601/1083

--- auth.js
+++ auth.js
@@ -1,3 +1,14 @@
+const passport = require("passport");
+const ObjectID = require("mongodb").ObjectID;
+
 module.exports = function (app, myDataBase) {
-  console.log("authentication files loaded");
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+
+  passport.deserializeUser(async (id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
 };
--- server.js
+++ server.js
@@ -38,66 +38,6 @@ myDB(async (client) => {
   const myDataBase = await client.db("database").collection("users");
   routes(app, myDataBase);
   auth(app, myDataBase);
-  
-  passport.serializeUser((user, done) => {
-    done(null, user._id);
-  });
-
-  passport.deserializeUser(async (id, done) => {
-    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-      done(null, doc);
-    });
-  });
-
-    function ensureAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-      return next();
-    }
-    res.redirect("/");
-  }
-
-  app.route("/register").post(
-    (req, res, next) => {
-      const hash = bcrypt.hashSync(req.body.password, 12);
-
-      myDataBase.findOne({ username: req.body.username }, (err, user) => {
-        if (err) {
-          next(err);
-        } else if (user) {
-          res.redirect("/");
-        } else {
-          myDataBase.insertOne(
-            {
-              username: hash,
-              password: req.body.password,
-            },
-            (err, doc) => {
-              if (err) {
-                res.redirect("/");
-              } else {
-                // The inserted document is held within
-                // the ops property of the doc
-                next(null, doc.ops[0]);
-              }
-            }
-          );
-        }
-      });
-    },
-    passport.authenticate("local", { failureRedirect: "/" }),
-    (req, res, next) => {
-      res.redirect("/profile");
-    }
-  );
-
-  app.route("/logout").get((req, res) => {
-    req.logout();
-    res.redirect("/");
-  });
-
-  app.use((req, res, next) => {
-    res.status(404).type("text").send("Not Found");
-  });
 
   passport.use(
     new LocalStrategy((username, password, done) => {
