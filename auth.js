0503c0e481639a304da9dad622341f6106a134c2
Glitch (spectacled-lofty-stingray) <none>
2023-02-08 21:37:38 +0100

    FCC Advanced Node Express App Final

--- auth.js
+++ auth.js
@@ -1,73 +1,63 @@
-"use strict";
-require("dotenv").config();
-const passport = require("passport");
-const ObjectID = require("mongodb").ObjectID;
-const LocalStrategy = require("passport-local");
-const bcrypt = require("bcrypt");
-const GitHubStrategy = require("passport-github").Strategy;
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+const LocalStrategy = require('passport-local');
+const GitHubStrategy = require('passport-github').Strategy;
+const { ObjectID } = require('mongodb');
 
 module.exports = function (app, myDataBase) {
+  passport.use(new LocalStrategy((username, password, done) => {
+    myDataBase.findOne({ username: username }, (err, user) => {
+      console.log(`User ${username} attempted to log in.`);
+      if (err) return done(err);
+      if (!user) return done(null, false);
+      // if (password !== user.password) return done(null, false);
+      if (!bcrypt.compareSync(password, user.password)) return done(null, false);
+      return done(null, user);
+    });
+  }));
+
   passport.serializeUser((user, done) => {
     done(null, user._id);
   });
-
-  passport.deserializeUser(async (id, done) => {
+  
+  passport.deserializeUser((id, done) => {
     myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
       done(null, doc);
     });
   });
 
-  passport.use(
-    new GitHubStrategy(
+  passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: 'https://spectacled-lofty-stingray.glitch.me/auth/github/callback',
+  }, (accessToken, refreshToken, profile, cb) => {
+    console.log(profile);
+    myDataBase.findOneAndUpdate(
+      { id: profile.id },
       {
-        clientID: process.env.GITHUB_CLIENT_ID,
-        clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL:
-          "https://spectacled-lofty-stingray.glitch.me/auth/github/callback",
+        $setOnInsert: {
+          id: profile.id,
+          username: profile.username,
+          name: profile.displayName || 'John Doe',
+          photo: profile.photos[0].value || '',
+          email: Array.isArray(profile.emails)
+            ? profile.emails[0].value
+            : 'No public email',
+          created_on: new Date(),
+          provider: profile.provider || '',
+        },
+        $set: {
+          last_login: new Date(),
+        },
+        $inc: {
+          login_count: 1,
+        },
       },
-      function (accessToken, refreshToken, profile, cb) {
-        console.log(profile);
-        //Database logic here with callback containing your user object
-        myDataBase.findOneAndUpdate(
-          { id: profile.id },
-          {
-            $setOnInsert: {
-              id: profile.id,
-              username: profile.username,
-              name: profile.displayName || "John Doe",
-              photo: profile.photos[0].value || "",
-              email: Array.isArray(profile.emails)
-                ? profile.emails[0].value
-                : "No public email",
-              created_on: new Date(),
-              provider: profile.provider || "",
-            },
-            $set: {
-              last_login: new Date(),
-            },
-            $inc: {
-              login_count: 1,
-            },
-          },
-          { upsert: true, new: true },
-          (err, doc) => {
-            return cb(null, doc.value);
-          }
-        );
+      { upsert: true, new: true },
+      (err, doc) => {
+        console.log(doc.value);
+        return cb(null, doc.value);
       }
-    )
-  );
-
-  passport.use(
-    new LocalStrategy((username, password, done) => {
-      myDataBase.findOne({ username: username }, (err, user) => {
-        console.log(`User ${username} attempted to log in.`);
-        if (err) return done(err);
-        if (!user) return done(null, false);
-        if (!bcrypt.compareSync(password, user.password))
-          return done(null, false);
-        return done(null, user);
-      });
-    })
-  );
-};
+    );
+  }));
+}
--- routes.js
+++ routes.js
@@ -1,27 +1,20 @@
 const passport = require("passport");
 const bcrypt = require("bcrypt");
 
+const ensureAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated()) return next();
+  return res.redirect("/");
+};
+
 module.exports = function (app, myDataBase) {
   app.route("/").get((req, res) => {
-    res.render("pug", {
-      message: "Please login",
+    res.render("index", {
+      title: "Connected to Database",
+      message: "Please log in",
       showLogin: true,
       showRegistration: true,
       showSocialAuth: true,
     });
-  });
-
-  app.route("/auth/github").get(passport.authenticate("github"));
-
-  app
-    .route("/auth/github/callback")
-    .get(passport.authenticate("github"), (req, res) => {
-      req.session.user_id = req.user.id;
-      res.redirect("/chat");
-    });
-
-  app.route("chat").get(ensureAuthenticated, (req, res) => {
-    res.render(process.cwd + "/views/pug/chat", { user: req.user });
   });
 
   app
@@ -29,63 +22,64 @@ module.exports = function (app, myDataBase) {
     .post(
       passport.authenticate("local", { failureRedirect: "/" }),
       (req, res) => {
-        res.redirect("/profile");
+        return res.redirect("/profile");
       }
     );
 
   app.route("/profile").get(ensureAuthenticated, (req, res) => {
-    res.render(process.cwd() + "/views/pug/profile", {
-      username: req.user.username,
-    });
+    return res.render("profile", { username: req.user.username });
+  });
+
+  app.route("/logout").get((req, res) => {
+    req.logout();
+    return res.redirect("/");
   });
 
   app.route("/register").post(
     (req, res, next) => {
-      const hash = bcrypt.hashSync(req.body.password, 12);
+      myDataBase.findOne({ username: req.body.username }, (err, foundUser) => {
+        if (err) next(err);
+        if (foundUser) return res.redirect("/");
 
-      myDataBase.findOne({ username: req.body.username }, (err, user) => {
-        if (err) {
-          next(err);
-        } else if (user) {
-          res.redirect("/");
-        } else {
-          myDataBase.insertOne(
-            {
-              username: hash,
-              password: req.body.password,
-            },
-            (err, doc) => {
-              if (err) {
-                res.redirect("/");
-              } else {
-                // The inserted document is held within
-                // the ops property of the doc
-                next(null, doc.ops[0]);
-              }
-            }
-          );
-        }
+        const hash = bcrypt.hashSync(req.body.password, 12);
+
+        myDataBase.insertOne(
+          {
+            username: req.body.username,
+            // password: req.body.password,
+            password: hash,
+          },
+          (err, doc) => {
+            if (err) return res.redirect("/");
+            // the inserted document is held within the ops property of the doc
+            next(null, doc.ops[0]);
+          }
+        );
       });
     },
     passport.authenticate("local", { failureRedirect: "/" }),
-    (req, res, next) => {
-      res.redirect("/profile");
+    (req, res) => {
+      return res.redirect("/profile");
     }
   );
 
-  app.route("/logout").get((req, res) => {
-    req.logout();
-    res.redirect("/");
+  app.route("/chat").get(ensureAuthenticated, (req, res) => {
+    return res.render("chat", { user: req.user });
   });
+
+  app.route("/auth/github").get(passport.authenticate("github"));
+
+  app
+    .route("/auth/github/callback")
+    .get(
+      passport.authenticate("github", { failureRedirect: "/" }),
+      (req, res) => {
+        req.session.user_id = req.user.id;
+        return res.redirect("/chat");
+      }
+    );
 
   app.use((req, res, next) => {
     res.status(404).type("text").send("Not Found");
   });
-
-  function ensureAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-      return next();
-    }
-    res.redirect("/");
-  }
 };
